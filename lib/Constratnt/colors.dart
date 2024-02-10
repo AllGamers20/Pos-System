afc14da3b7c7e4a0020084655acd179a5ad51ccf
AllGamers20 <husseinmoubarackmonster@gmail.com>
2024-02-10 22:50:00 +0200

    first commit

---
+++ lib/Constratnt/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color textColor = Color(0xFF6a3e37);
+const Color primaryColor = Color(0xFF29e7cd);
+const Color lightColor = Color(0xFFf4ebe8);
+const Color secondaryColor = Color(0xFF3993dd);
+const Color iconColor = Color(0xFFe0acd5);
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:possystem/Constratnt/colors.dart';
+import 'package:possystem/Screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
